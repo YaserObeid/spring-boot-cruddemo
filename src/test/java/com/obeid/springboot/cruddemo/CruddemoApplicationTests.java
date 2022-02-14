de91b35396bb99e21e72fd2c5012f973ce5cbdf3
YaserObeid <yaser.obeid@hotmail.com>
2022-02-14 13:18:31 +0100

    init commit

---
+++ src/test/java/com/obeid/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obeid.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
